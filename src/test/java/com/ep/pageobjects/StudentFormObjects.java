6355a838a648451c6aeac2e4f1f41ff72f7ce809
Manikanta451 <mani6747@gmail.com>
2016-09-14 19:03:33 +0530

    new objects are added

--- src/test/java/com/ep/pageobjects/StudentFormObjects.java
+++ src/test/java/com/ep/pageobjects/StudentFormObjects.java
@@ -3,7 +3,6 @@ package com.ep.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import com.ep.pagefactory.CommonBase;
 
 public class StudentFormObjects extends CommonBase {
@@ -15,6 +14,16 @@ public class StudentFormObjects extends CommonBase {
 	
 	public static WebElement newstudentform(WebDriver driver){
 		webelement = driver.findElement(By.xpath("html/body/div[1]/section[1]/div/div/form/div/div/div/div[2]/div/div/div/div/p/a[1]"));
+        return webelement;
+        }
+	
+	public static WebElement beforeandafter(WebDriver driver){
+		webelement = driver.findElement(By.xpath("html/body/div[1]/section[1]/div/div/div/div/form/div/div[2]/div/div/div[1]/div/div[3]/input"));
+        return webelement;
+        }
+	
+	public static WebElement schoolname(WebDriver driver){
+		webelement = driver.findElement(By.xpath("html/body/div[1]/section[1]/div/div/div/div/form/div/div[2]/div/div/div[2]/div[1]/select"));
         return webelement;
         }
 
