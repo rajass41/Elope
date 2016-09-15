082d6963dca1475799d95481d48650566e5f5833
Manikanta451 <mani6747@gmail.com>
2016-09-15 12:43:19 +0530

    New data is added

--- src/test/java/com/ep/datainitialization/DataInt.java
+++ src/test/java/com/ep/datainitialization/DataInt.java
@@ -8,12 +8,42 @@ public class DataInt implements Serializable {
 	private static final long serialVersionUID = -5841500346110682370L;
 	
 
-	// ---Login------//
+					// ---Login------//
 	
 					public String username = "";
 					public String userpassword = "";
 					
-	//----Login getter setters-------??				
+	
+					//----StudentForm----//
+							
+					public String Lastname ="";
+					public String Firstname ="";
+					public String Middlename ="";
+					public String SSN ="";
+					public String Grade ="";
+					public String Studentid ="";
+					public String DOB="";
+					public String Address="";
+					public String Zip ="";
+					public String Home ="";
+					public String Cell ="";
+					public String Emailaddress ="";
+					public String Parentemployer ="";
+					public String Telephone ="";
+					public String Name ="";
+					public String Relationtochild ="";
+					public String Pickuppersonname="";
+					public String PickuppersonTelephone ="";
+					public String ParentorGuardian ="";
+					public String SignatureLegalParentorGuardian ="";
+ 
+					
+					
+					
+					
+					
+					
+	//----Login getter setters-------//			
 					
 					public String getUsername() {
 						return username;
@@ -28,6 +58,133 @@ public class DataInt implements Serializable {
 						this.userpassword = userpassword;
 					}
 					
+					
+					
+	//------StudentForm Getter setter----//
+					
+					
+					public String getLastname() {
+						return Lastname;
+					}
+					public void setLastname(String lastname) {
+						Lastname = lastname;
+					}
+					public String getFirstname() {
+						return Firstname;
+					}
+					public void setFirstname(String firstname) {
+						Firstname = firstname;
+					}
+					public String getMiddlename() {
+						return Middlename;
+					}
+					public void setMiddlename(String middlename) {
+						Middlename = middlename;
+					}
+					public String getSSN() {
+						return SSN;
+					}
+					public void setSSN(String sSN) {
+						SSN = sSN;
+					}
+					public String getGrade() {
+						return Grade;
+					}
+					public void setGrade(String grade) {
+						Grade = grade;
+					}
+					public String getStudentid() {
+						return Studentid;
+					}
+					public void setStudentid(String studentid) {
+						Studentid = studentid;
+					}
+					public String getDOB() {
+						return DOB;
+					}
+					public void setDOB(String dOB) {
+						DOB = dOB;
+					}
+					public String getAddress() {
+						return Address;
+					}
+					public void setAddress(String address) {
+						Address = address;
+					}
+					public String getZip() {
+						return Zip;
+					}
+					public void setZip(String zip) {
+						Zip = zip;
+					}
+					public String getHome() {
+						return Home;
+					}
+					public void setHome(String home) {
+						Home = home;
+					}
+					public String getCell() {
+						return Cell;
+					}
+					public void setCell(String cell) {
+						Cell = cell;
+					}
+					public String getEmailaddress() {
+						return Emailaddress;
+					}
+					public void setEmailaddress(String emailaddress) {
+						Emailaddress = emailaddress;
+					}
+					public String getParentemployer() {
+						return Parentemployer;
+					}
+					public void setParentemployer(String parentemployer) {
+						Parentemployer = parentemployer;
+					}
+					public String getTelephone() {
+						return Telephone;
+					}
+					public void setTelephone(String telephone) {
+						Telephone = telephone;
+					}
+					public String getName() {
+						return Name;
+					}
+					public void setName(String name) {
+						Name = name;
+					}
+					public String getRelationtochild() {
+						return Relationtochild;
+					}
+					public void setRelationtochild(String relationtochild) {
+						Relationtochild = relationtochild;
+					}
+					public String getPickuppersonname() {
+						return Pickuppersonname;
+					}
+					public void setPickuppersonname(String pickuppersonname) {
+						Pickuppersonname = pickuppersonname;
+					}
+					public String getPickuppersonTelephone() {
+						return PickuppersonTelephone;
+					}
+					public void setPickuppersonTelephone(String pickuppersonTelephone) {
+						PickuppersonTelephone = pickuppersonTelephone;
+					}
+					public String getParentorGuardian() {
+						return ParentorGuardian;
+					}
+					public void setParentorGuardian(String parentorGuardian) {
+						ParentorGuardian = parentorGuardian;
+					}
+					public String getSignatureLegalParentorGuardian() {
+						return SignatureLegalParentorGuardian;
+					}
+					public void setSignatureLegalParentorGuardian(
+							String signatureLegalParentorGuardian) {
+						SignatureLegalParentorGuardian = signatureLegalParentorGuardian;
+					}
+										
 }	
 					
 	
