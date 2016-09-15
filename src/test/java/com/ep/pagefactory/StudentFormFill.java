5b0e569f965ffc94ccd9ff4512e5407a237fce7d
Manikanta451 <mani6747@gmail.com>
2016-09-15 12:46:30 +0530

    Functionality is added

--- src/test/java/com/ep/pagefactory/StudentFormFill.java
+++ src/test/java/com/ep/pagefactory/StudentFormFill.java
@@ -9,20 +9,56 @@ import com.ep.pageobjects.StudentFormObjects;
 
 public class StudentFormFill extends CommonBase {
 
-	protected StudentFormFill(WebDriver driver) {
+	public StudentFormFill(WebDriver driver) {
 		super(driver);
-		
 	}
 	
 
-	public void formfill(DataInt dataInt) throws Exception {
+	public void newformfill(DataInt dataInt) throws Exception {
 		try {
-			Thread.sleep(6000);
+			Thread.sleep(8000);
 			StudentFormObjects.newstudentform(driver).click();
 			Thread.sleep(5000);
 			StudentFormObjects.beforeandafter(driver).click();
 			Thread.sleep(2000);
 			new Select(StudentFormObjects.schoolname(driver)).selectByVisibleText("Balmoral Ridgeway Elementary");
+			Thread.sleep(2000);
+			StudentFormObjects.lastname(driver).sendKeys(dataInt.getLastname());
+			StudentFormObjects.firsttname(driver).sendKeys(dataInt.getFirstname());
+			StudentFormObjects.middletname(driver).sendKeys(dataInt.getMiddlename());
+			StudentFormObjects.ssn(driver).sendKeys(dataInt.getSSN());
+			StudentFormObjects.grade(driver).sendKeys(dataInt.getGrade());
+			StudentFormObjects.studentId(driver).sendKeys(dataInt.getStudentid());
+			StudentFormObjects.dateofbirth(driver).sendKeys(dataInt.getDOB());
+			Thread.sleep(1000);
+			StudentFormObjects.gender(driver).click();
+			StudentFormObjects.ImmunizationRecordatSchool(driver).click();
+			StudentFormObjects.studenthavea504(driver).click();
+			StudentFormObjects.IEP(driver).click();
+			StudentFormObjects.medical(driver).click();
+			StudentFormObjects.adress(driver).sendKeys(dataInt.getAddress());
+			StudentFormObjects.zip(driver).sendKeys(dataInt.getZip());
+			StudentFormObjects.home(driver).sendKeys(dataInt.getHome());
+			StudentFormObjects.cell(driver).sendKeys(dataInt.getCell());
+			StudentFormObjects.email(driver).sendKeys(dataInt.getEmailaddress());
+			StudentFormObjects.parentemployee(driver).sendKeys(dataInt.getParentemployer());
+			StudentFormObjects.telephone(driver).sendKeys(dataInt.getTelephone());
+			StudentFormObjects.name(driver).sendKeys(dataInt.getName());
+			StudentFormObjects.relationtochild(driver).sendKeys(dataInt.getRelationtochild());
+			StudentFormObjects.cell1(driver).sendKeys(dataInt.getCell());
+			StudentFormObjects.email1(driver).sendKeys(dataInt.getEmailaddress());
+			StudentFormObjects.pickuppersonname(driver).sendKeys(dataInt.getName());
+			StudentFormObjects.telephone1(driver).sendKeys(dataInt.getTelephone());
+			StudentFormObjects.medicalcondition(driver).click();
+			StudentFormObjects.parentorgurdain(driver).sendKeys(dataInt.getParentorGuardian());
+			StudentFormObjects.signature(driver).sendKeys(dataInt.getSignatureLegalParentorGuardian());
+			StudentFormObjects.date(driver).click();
+			Thread.sleep(1000);
+			super.systemdateselect();
+			StudentFormObjects.terms(driver).click();
+			Thread.sleep(3000);
+			//StudentFormObjects.submit(driver).click();
+			
 		} catch (Exception e) {
 			excep = e.toString();
 			Assert.fail(excep);
